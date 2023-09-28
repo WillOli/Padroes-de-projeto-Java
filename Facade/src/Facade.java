f984f2d6ceeb65860d2ff6a1da0a879c8b0ed607
WillOli <william.ika@gmail.com>
2023-09-28 09:42:20 -0300

    Praticando com Java puro: Facade

---
+++ Facade/src/Facade.java
@@ -0,0 +1,11 @@
+import subSistema1.crm.CrmService;
+import subSistema2.cep.CepApi;
+
+public class Facade {
+    public void migrationCliente(String nome, String cep) {
+        String cidade = CepApi.getInstance().recuperarCidade(cep);
+        String estado = CepApi.getInstance().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
+}
---
+++ Facade/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Facade facede = new Facade();
+        facede.migrationCliente("Santa Tereza", "2010220");
+    }
+}
